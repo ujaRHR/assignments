53562bc870a3c739b3d7cddf1492dd1b31a1b4d5
Reajul Hasan Raju <rhraju.dev@gmail.com>
2023-11-19 23:46:28 +0600

    Module 8/ Laravel Request, Response & Cookies

---
+++ module_8/request-response/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// id - only numeric value is acceptable
+Route::get('/profile/{id}', [ProfileController::class, 'index'])->whereNumber('id');
